5e9b68ad68a9d715dca0d221797ea9fc29467b99
Dylan Llewellyn <46717769+herefishyfish@users.noreply.github.com>
2022-12-05 12:13:44 +0800

    fix: make isomorphic ws use browser module

--- packages/nativescript-rxdb/nativescript.webpack.js
+++ packages/nativescript-rxdb/nativescript.webpack.js
@@ -8,6 +8,7 @@ module.exports = (webpack) => {
     config.resolve.alias.set('ws', resolve(nodeModulesPath, '@valor/nativescript-websockets'));
     config.resolve.alias.set('pouchdb-md5', resolve(nodeModulesPath, '@herefishyfish/rxdb/pouchdb'));
     config.resolve.alias.set('uuid', resolve(nodeModulesPath, '@herefishyfish/rxdb/uuid'));
+    config.resolve.alias.set('isomorphic-ws', resolve(appRoot.path, './node_modules/isomorphic-ws/browser.js'));
     config.resolve.alias.set('isomorphic-fetch', resolve(nodeModulesPath, '@nativescript/core'));
     config.resolve.alias.set('broadcast-channel', resolve(appRoot.path, './node_modules/broadcast-channel/dist/esbrowser'));
     config.resolve.alias.set(resolve(appRoot.path, './node_modules/unload/dist/es/browser.js'), resolve(nodeModulesPath, '@herefishyfish/nativescript-rxdb/unload/browser.js'));
