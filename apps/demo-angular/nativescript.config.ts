bda9e1fa695d82a2bebe0094f0365064eec66aef
Dylan Llewellyn <46717769+herefishyfish@users.noreply.github.com>
2023-02-01 17:45:51 +0800

    feat: add sqlite options

--- apps/demo-angular/nativescript.config.ts
+++ apps/demo-angular/nativescript.config.ts
@@ -1,14 +1,15 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-	id: 'org.nativescript.plugindemoangular',
-	appResourcesPath: '../../tools/assets/App_Resources',
-	android: {
-		v8Flags: '--expose_gc',
-		markingMode: 'none',
-	},
-	appPath: 'src',
+  id: 'org.nativescript.plugindemoangular',
+  appResourcesPath: '../../tools/assets/App_Resources',
+  android: {
+    v8Flags: '--expose_gc',
+    markingMode: 'none',
+    enableMultithreadedJavascript: true,
+  },
+  appPath: 'src',
   cli: {
-    packageManager: 'npm' 
-  }
+    packageManager: 'npm',
+  },
 } as NativeScriptConfig;
--- packages/nativescript-sqlite-rxstorage-adapter/index.ts
+++ packages/nativescript-sqlite-rxstorage-adapter/index.ts
@@ -1,24 +1,21 @@
 type SQLiteQueryWithParams = any;
 import { knownFolders, path } from '@nativescript/core';
 import { openOrCreate, SQLiteDatabase } from '@herefishyfish/requery-sqlite';
-import { SQLiteBasics } from 'rxdb-premium/dist/lib/plugins/sqlite';
+import { SQLiteBasics } from 'rxdb-premium/plugins/sqlite';
 
-export interface SQLiteOptions {
-  threading?: boolean;
+export interface ISQLiteDatabaseAdapterOptions {
   transformBlobs?: boolean;
+  threading?: boolean;
   flags?: number;
 }
 
-export const getSQLiteBasicsNativeScript = (options?: SQLiteOptions): SQLiteBasics<SQLiteDatabase> => {
+export const getSQLiteBasicsNativeScript = (options?: ISQLiteDatabaseAdapterOptions): SQLiteBasics<SQLiteDatabase> => {
   return {
     open: async (name: string) => {
-      return await openOrCreate(
-        path.join(knownFolders.documents().getFolder('db').path, `${name}.sqlite`),
-        options ?? {
-          threading: true,
-          transformBlobs: true,
-        }
-      );
+      return await openOrCreate(path.join(knownFolders.documents().getFolder('db').path, `${name}.sqlite`), {
+        transformBlobs: true,
+        ...(options ?? {}),
+      });
     },
     all: async (db: SQLiteDatabase, queryWithParams: SQLiteQueryWithParams) => {
       return (await db.select(queryWithParams.query, queryWithParams.params ?? [])) as any;
